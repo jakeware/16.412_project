3ca4012db4519ed26d1b420c6042a0a71580332a
Jake Ware <jakeware@gmail.com>
2015-04-03 15:51:54 -0400

    added comments.

--- mdp_grid_world/Reward.m
+++ mdp_grid_world/Reward.m
@@ -1,4 +1,15 @@
-function reward = Reward(s,a)
+%% Summary
+% This function takes the current state and selected action and returns the
+% reward for that state, action pair.
+
+%% Inputs
+% s: state is a 1x2 tuple of reals.
+% a: actions is a 1x2 tuple of reals.
+
+%% Outputs
+% r: reward is a real number.
+
+function r = Reward(s,a)
     % Rewards
     % 1: free
     % 5: ice
@@ -10,5 +21,5 @@ function reward = Reward(s,a)
         1,50,5,5;
         1,1,1,1];
     
-    reward = R(s(1),s(2));
+    r = R(s(1),s(2));
 end
