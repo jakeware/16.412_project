2f23e67248ee32061d8fb4e7a1ed5621dc404a52
Benoit Landry <landry@mit.edu>
2015-04-03 15:58:42 -0400

    skeleton of the MDP solver

--- mdp_grid_world/main.m
+++ mdp_grid_world/main.m
@@ -6,34 +6,36 @@ clc
 % This is a test of a MDP solver on a grid world
 
 %% Setup
-n = 4;  % number of states
+n = 4;  % size of the grid
 gamma = 0.9;  % dicount factor
+epsilon = .01; % convergence treshold
+%goal_state = [4 4]; get it from the reward?
 
-% latent initial state
-s0 = [1,1];
-
-% initial belief
-b0 = zeros(n);
-b0(s0(1),s0(2)) = 1;
-
-% States
-% S \in {x,y}
-% (1,n),...,(n,n);
-% ...,...,...;
-% (1,1),...,(n,1);
-
-%% Check
-for i=1:n
-    for j=1:n
-        r_out(i,j) = Reward([i,j],0);
-    end
-end
+V = rand(n);
+V_diff = Inf*ones(size(V));
+[X,Y] = meshgrid(1:n,1:n);
 
 %% Analysis
-
-
-%% Plot
-figure
-imagesc(r_out)
-xlabel('x')
-ylabel('y')
+while max(max(V_diff))>epsilon
+  V_new = zeros(size(V));
+  for k=1:numel(X)
+    i = X(k);
+    j = Y(k);
+    if (i==goal_state(1)&&j==goal_state(2))
+      V_new(i,j) = R(i,j);
+      continue;
+    end
+    actions = % get a list of actions it can do in state (i,j)
+    new_val = zeros(size(actions));
+    for ai=1:numel(actions)
+      act = actions(ai);
+      % get the probably of each next state possible with act
+      % compute gamma*dot(prob_dist,V(states of the prob dist))
+      % new_val(ai) = the new value
+    end
+    V_new(i,j) = max(new_val) + R(i,j);
+  end
+  V_diff = abs(V_new-V);
+  V = V_new;
+  imagesc(V);
+end
