dac09d5bdf76a590cd727f8d5bd8de980aa325c4
Jake Ware <jakeware@gmail.com>
2015-04-03 15:51:32 -0400

    added comments.

--- mdp_grid_world/Actions.m
+++ mdp_grid_world/Actions.m
@@ -1,33 +1,45 @@
-function actions = Actions(state,n)
+%% Summary
+% This function takes the current state and returns the viable actions.
+% Note that it takes the size of the grid to check for viability.
+
+%% Inputs
+% s: the state is a 1x2 tuple of reals
+% n: the grid dimension as a real number
+
+%% Outputs
+% a: the actions is a mx2 matrix of reals with each row being a viable 
+% action.  Here, m is the number of viable actions.
+
+function a = Actions(s,n)
     % field
-    if (state(1) > 1 && state(1) <n && state(2) > 1 && state(2) < n)
-        actions = [1,0;0,1;-1,0;0,-1];
+    if (s(1) > 1 && s(1) <n && s(2) > 1 && s(2) < n)
+        a = [1,0;0,1;-1,0;0,-1];
     % lower left
-    elseif (state(1) == 1 && state(2) == 1)
-        actions = [1,0;0,1];
+    elseif (s(1) == 1 && s(2) == 1)
+        a = [1,0;0,1];
     % upper left
-    elseif (state(1) == 1 && state(2) == n)
-        actions = [-1,0;0,-1];
+    elseif (s(1) == 1 && s(2) == n)
+        a = [-1,0;0,-1];
     % lower right
-    elseif (state(1) == n && state(2) == 1)
-        actions = [1,0;0,-1];
+    elseif (s(1) == n && s(2) == 1)
+        a = [1,0;0,-1];
     % upper right
-    elseif (state(1) == n && state(2) == n)
-        actions = [-1,0;0,-1];
+    elseif (s(1) == n && s(2) == n)
+        a = [-1,0;0,-1];
     % left edge
-    elseif (state(1) == 1 && state(2) > 1 && state(2) < n)
-        actions = [1,0;0,1;0,-1];
+    elseif (s(1) == 1 && s(2) > 1 && s(2) < n)
+        a = [1,0;0,1;0,-1];
     % right edge
-    elseif (state(1) == n && state(2) > q && state(2) < n)
-        actions = [0,1;-1,0;0,-1];
+    elseif (s(1) == n && s(2) > q && s(2) < n)
+        a = [0,1;-1,0;0,-1];
     % lower edge
-    elseif (state(2) == 1 && state(1) > 1 && state(1) < n)
-        actions = [1,0;0,1;-1,0];
+    elseif (s(2) == 1 && s(1) > 1 && s(1) < n)
+        a = [1,0;0,1;-1,0];
     % upper edge
-    elseif (state(2) == n && state(1) > 1 && state(1) < n)
-        actions = [1,0;-1,0;0,-1];
+    elseif (s(2) == n && s(1) > 1 && s(1) < n)
+        a = [1,0;-1,0;0,-1];
     % else
     else
-        fprintf('Unrecognized Action {%i,%i,%i}',state(1),state(2),n);
+        fprintf('Unrecognized Action {%i,%i,%i}',s(1),s(2),n);
     end
 end
