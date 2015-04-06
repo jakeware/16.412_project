cc39af7649841ce17716f35cbcff5f64710d9d2a
Jake Ware <jakeware@gmail.com>
2015-04-06 18:46:53 -0400

    implemented probabilistic transition function.  have not fully tested it yet.

--- mdp_grid_world/Transition.m
+++ mdp_grid_world/Transition.m
@@ -17,10 +17,25 @@ function t = Transition(s,a,n)
     % 10% stay still
     % 10% overshoot
     
+    % new state (stay,intended action,overshoot)
+    s_p = repmat(s,3,1) + [zeros(1,2);a;2*a];
+    
     % check overshoot
-    %s_p = state + [zeros(n,1);a;2*a];
-    %if (s_p(1,3) < 1 || s_p(1,3) > n || s_p(2,3) < 1 || s_p(2,3) > n)
-        
-    s_p = s + a;
-    t = [s_p,1];
+    if (s_p(3,1) < 1 || s_p(3,1) > n || s_p(3,2) < 1 || s_p(3,2) > n)
+        % check action
+        if (s_p(2,1) < 1 || s_p(2,1) > n || s_p(2,2) < 1 || s_p(2,2) > n)
+            % return only the stay action
+            t = [s_p(1,:),1];
+        else
+            % return stay,intended
+            t = [s_p(1:2,:),[0.1;0.9]];
+        end
+    else
+        % return stay,intended,overshoot
+        t = [s_p,[0.1;0.8;0.1]];
+    end
+    
+    % ignore probabilities
+    %s_p = s + a;
+    %t = [s_p,1];
 end
