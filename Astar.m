5971d6e37570273dddc86fad1ac35841e4d7e520
Jake Ware <jakeware@gmail.com>
2015-05-06 11:19:08 -0400

    added priority queue.  velocity guard not working yet.

--- Astar.m
+++ Astar.m
@@ -1,25 +1,31 @@
-function traj = Astar(start,goal,n,speeds,obs)
+function [xtraj,gtraj,htraj] = Astar(start,goal,speeds,obs,wind)
 
-  alpha = 0;
+  alpha = .5;
 
+  n = size(obs,1);
+  
   start(3) = find(speeds==start(3));
   start(4) = find(speeds==start(4));
   goal(3) = find(speeds==goal(3));
   goal(4) = find(speeds==goal(4));
-	s = numel(speeds);
+  s = numel(speeds);
   
-  closedset = [];
-	openset = [start'];
+  expanded_states = zeros(n,n,s,s);
   came_from_row = -1*ones(n,n,s,s);
   came_from_col = -1*ones(n,n,s,s);
   came_from_row_spe = -1*ones(n,n,s,s);
   came_from_col_spe = -1*ones(n,n,s,s);
   
-	g_score = Inf*ones(n,n,s,s);
+  g_score = Inf*ones(n,n,s,s);
   g_score(start(1),start(2),start(3),start(4)) = 0;
-	f_score = Inf*ones(n,n,s,s);
+  f_score = Inf*ones(n,n,s,s);
   f_score(start(1),start(2),start(3),start(4)) = alpha*g_score(start(1),start(2),start(3),start(4)) + (1-alpha)*heuristic_cost_estimate(start,goal,speeds);
-
+  openset = start;
+  opensetpq = pq_create(n*n*s*s);
+  in_openset = zeros(n,n,s,s);
+  in_openset(start(1),start(2),start(3),start(4)) = 1;
+  pq_push(opensetpq,1,-f_score(start(1),start(2),start(3),start(4)));
+  
   h = figure(55);
   hold on;
   xlim([0 100]);
@@ -28,79 +34,86 @@ function traj = Astar(start,goal,n,speeds,obs)
   
   while (size(openset,2)>0)
     
-    % this needs to be optimized, it looks for the lowest f_score
-    current = openset(:,1);
-    current_i = 1;
-    min_f = f_score(current(1),current(2),current(3),current(4));
-    for i=2:size(openset,2);
-      current_candidate = openset(:,i);
-      if f_score(current_candidate(1),current_candidate(2),current_candidate(3),current_candidate(4))<min_f
-        min_f = f_score(current_candidate(1),current_candidate(2),current_candidate(3),current_candidate(4));
-        current = current_candidate;
-        current_i = i;
-      end
-    end
+    %current = openset(:,1);
+    %current_i = 1;
+    %min_f = f_score(current(1),current(2),current(3),current(4));
+    %for i=2:size(openset,2);
+    %  current_candidate = openset(:,i);
+    %  if f_score(current_candidate(1),current_candidate(2),current_candidate(3),current_candidate(4))<min_f
+    %    min_f = f_score(current_candidate(1),current_candidate(2),current_candidate(3),current_candidate(4));
+    %    current = current_candidate;
+    %    current_i = i;
+    %  end
+    %end
+    [current_i,min_f] = pq_pop(opensetpq);
+    current = openset(:,current_i);
+    in_openset(current(1),current(2),current(3),current(4)) = 0;
     
-    figure(h);
-    plot3(current(1),current(2),10*norm([current(3),current(4)]),'*r');
+    %figure(h);
+    %plot3(current(1),current(2),10*norm([current(3),current(4)]),'*r');
     %pause;
     
     if all(current(:)==goal(:))
-      traj = reconstruct_path(came_from_row,came_from_col,came_from_row_spe,came_from_col_spe,start,goal,speeds);
+      [xtraj,gtraj,htraj] = reconstruct_path(came_from_row,came_from_col,came_from_row_spe,came_from_col_spe,start,goal,speeds,g_score);
       return;
     end
     
-    openset(:,current_i) = [];
-    closedset = [closedset,current];
-    neighs = get_neighbours(current,n,speeds,obs);
+    expanded_states(current(1),current(2),current(3),current(4)) = 1;
+    neighs = get_neighbours(current,n,speeds,obs,wind);
     for i=1:size(neighs,2)
       neigh = neighs(:,i);
-      if ismember(neigh',closedset','rows')
+      if expanded_states(neigh(1),neigh(2),neigh(3),neigh(4))
         continue;
       end
       tentative_g_score = g_score(current(1),current(2),current(3),current(4)) + dist_between(current,neigh,speeds);
-      if ~ismember(neigh',openset','rows') || tentative_g_score < g_score(neigh(1),neigh(2),neigh(3),neigh(4))
+      if ~in_openset(neigh(1),neigh(2),neigh(3),neigh(4)) || tentative_g_score < g_score(neigh(1),neigh(2),neigh(3),neigh(4))
         came_from_row(neigh(1),neigh(2),neigh(3),neigh(4)) = current(1);
         came_from_col(neigh(1),neigh(2),neigh(3),neigh(4)) = current(2);
         came_from_row_spe(neigh(1),neigh(2),neigh(3),neigh(4)) = current(3);
         came_from_col_spe(neigh(1),neigh(2),neigh(3),neigh(4)) = current(4);
         g_score(neigh(1),neigh(2),neigh(3),neigh(4)) = tentative_g_score;
         f_score(neigh(1),neigh(2),neigh(3),neigh(4)) = alpha*g_score(neigh(1),neigh(2),neigh(3),neigh(4)) + (1-alpha)*heuristic_cost_estimate(neigh,goal,speeds);
-        if ~ismember(neigh',openset','rows')
+        if ~in_openset(neigh(1),neigh(2),neigh(3),neigh(4))
           openset = [openset,neigh];
+          in_openset(neigh(1),neigh(2),neigh(3),neigh(4)) = 1;
+          pq_push(opensetpq,size(openset,2),-f_score(neigh(1),neigh(2),neigh(3),neigh(4)));
         end
       end
     end
   end
   
-  traj = 0;
+  xtraj = 0;
+  gtraj = 0;
+  htraj = 0;
 end
 
 function h = heuristic_cost_estimate(state,goal,speeds)
-  h = norm([goal(1) goal(2)]-[state(1) state(2)]);
+  h = norm(goal(1:2)-(state(1:2)+[speeds(state(3));speeds(state(4))]));
 end
 
 function cost = dist_between(state1,state2,speeds)
-  %cost = norm((state2(1:2)-state1(1:2))./eps([speeds(state1(3));speeds(state1(4))]));
-  %cost = (state2(1)-state1(1))/eps(speeds(state1(3))) + (state2(2)-state1(2))/eps(speeds(state1(4)));
   cost = norm(state2(1:2)-state1(1:2));
 end
 
-function traj = reconstruct_path(came_from_row,came_from_col,came_from_row_spe,came_from_col_spe,start,goal,speeds)
-  traj = [goal'];
-  current = goal';
+function [xtraj,gtraj,htraj] = reconstruct_path(came_from_row,came_from_col,came_from_row_spe,came_from_col_spe,start,goal,speeds,g_score)
+  xtraj = [goal];
+  gtraj = g_score(goal(1),goal(2),goal(3),goal(4));
+  htraj = heuristic_cost_estimate(goal,goal,speeds);
+  current = goal;
   while ~all(current(:)==start(:))
     current = [came_from_row(current(1),current(2),current(3),current(4));came_from_col(current(1),current(2),current(3),current(4));...
       came_from_row_spe(current(1),current(2),current(3),current(4));came_from_col_spe(current(1),current(2),current(3),current(4))];
-    traj = [current,traj];
+    xtraj = [current,xtraj];
+    gtraj = [g_score(current(1),current(2),current(3),current(4)),gtraj];
+    htraj = [heuristic_cost_estimate(current,goal,speeds),htraj];
   end
-  % converting speed indices to speeds
-  for j=1:size(traj,2)
-    traj(3:4,j) = [speeds(traj(3,j));speeds(traj(4,j))];
+  % converting speed indices to physicals speeds
+  for j=1:size(xtraj,2)
+    xtraj(3:4,j) = [speeds(xtraj(3,j));speeds(xtraj(4,j))];
   end
 end
 
-function clean_neighs = get_neighbours(state,n,speeds,obs)
+function clean_neighs = get_neighbours(state,n,speeds,obs,wind)
   if state(3)==1
     newrowspeed = 1:2;
   elseif state(3)==numel(speeds)
@@ -118,13 +131,32 @@ function clean_neighs = get_neighbours(state,n,speeds,obs)
   neighs = [];
   for rowspeedi=1:numel(newrowspeed)
     for colspeedi=1:numel(newcolspeed)
+
+      if state(1)+speeds(state(3))>n || state(1)+speeds(state(3))<1 || state(2)+speeds(state(4))>n || state(2)+speeds(state(4))<1 || obs(state(1)+speeds(state(3)),state(2)+speeds(state(4)))==1
+          continue;
+      end
+      
+      groundspeed = [speeds(newrowspeed(rowspeedi)) speeds(newcolspeed(colspeedi))];
+
+      if norm(groundspeed)>max(speeds)
+          continue;
+      end
+      airspeed = zeros(1,3);
+      airspeed(1) = groundspeed(1) - wind(state(1)+speeds(state(3)),state(2)+speeds(state(4)),1);
+      airspeed(2) = groundspeed(2) - wind(state(1)+speeds(state(3)),state(2)+speeds(state(4)),2);
+      airspeed(3) = -wind(state(1)+speeds(state(3)),state(2)+speeds(state(4)),3);
+      if norm(airspeed)>3
+        continue
+      end
       neighs = [neighs,[state(1)+speeds(state(3));state(2)+speeds(state(4));newrowspeed(rowspeedi);newcolspeed(colspeedi)]];
     end
   end
-  clean_neighs = [];
-  for i=1:size(neighs,2)
-    if neighs(1,i)<=n && neighs(1,i)>=1 && neighs(2,i)<=n && neighs(2,i)>=1 && obs(neighs(2,i),neighs(1,i))==0
-      clean_neighs = [clean_neighs,neighs(:,i)];
-    end
-  end
+  
+  %clean_neighs = [];
+  %for i=1:size(neighs,2)
+  %  if neighs(1,i)<=n && neighs(1,i)>=1 && neighs(2,i)<=n && neighs(2,i)>=1 && obs(neighs(2,i),neighs(1,i))==0
+  %    clean_neighs = [clean_neighs,neighs(:,i)];
+  %  end
+  %end
+  
 end
--- runAstar.m
+++ runAstar.m
@@ -1,11 +1,39 @@
-load('celltype.mat')
-obs = celltype{1}(:,:,2)==0;
+z = 2;
 
+load('celltype.mat')
+obs = celltype{1}(:,:,z)==0;
 n = size(obs,1);
-speeds = [-1 0 1];
-start = [82 22 0 0];
-goal = [90 75 0 0];
-%goal = [82 30 0 0];
+
+inf_obs = zeros(n);
+rad = 2;
+for i=1:n
+    for j=1:n
+        found = 0;
+        ioffset = -rad:rad;
+        joffset = -rad:rad;
+        for ii=1:numel(ioffset)
+           for jj=1:numel(joffset)
+              to_check = [i,j]+[ioffset(ii),joffset(jj)];
+              if to_check(1)>n || to_check(1)<1 || to_check(2)>n || to_check(2)<1
+                  continue;
+              else
+                 found = found | obs(to_check(1),to_check(2));
+              end
+           end
+        end
+        inf_obs(i,j) = found;
+    end
+end
+obs = inf_obs;
+
+load('velocity.mat')
+wind(:,:,1) = velocity.u{1}(:,:,z);
+wind(:,:,2) = velocity.v{1}(:,:,z);
+wind(:,:,3) = velocity.w{1}(:,:,z);
+
+speeds = [-2 -1 0 1 2];
+start = [82 22 0 0]';
+goal = [15 75 0 0]';
 
 figure(75);
 hold on;
@@ -14,14 +42,17 @@ plot(start(1),start(2),'bo');
 plot(goal(1),goal(2),'ro');
 
 display('running A*...');
-traj = Astar(start,goal,n,speeds,obs);
+tic;
+[xtraj,gtraj,htraj] = Astar(start,goal,speeds,obs,wind);
+toc
 display('done.');
 
 figure(75);
 hold on;
-vtraj = sqrt(sum(traj(3:4,:).^2,1));
-imagesc(obs*max(vtraj)*1.5);
-scatter(traj(1,:),traj(2,:),25,vtraj,'filled');
+vtraj = sqrt(sum(xtraj(3:4,:).^2,1));
+%imagesc(obs*max(vtraj)*1.5);
+imagesc(velocity.mag{1}(:,:,z));
+scatter(xtraj(1,:),xtraj(2,:),25,vtraj,'filled');
 xlim([0 n]);
 ylim([0 n]);
 axis equal;
@@ -29,3 +60,7 @@ axis equal;
 figure(76);
 plot(vtraj,'b.-');
 ylim([min(speeds) max(speeds)]);
+
+figure(77);
+plot(1:numel(gtraj),gtraj,1:numel(htraj),htraj,'*');
+legend('G traj', 'H traj');
