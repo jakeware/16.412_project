2a2acc9491493cdb5ecf17bd169c48b94254478b
Jake Ware <jakeware@gmail.com>
2015-05-11 17:31:33 -0400

    made some plotting changes for the report.

--- wind_world/runAstar.m
+++ wind_world/runAstar.m
@@ -6,7 +6,7 @@ z = 2;
 
 addpath('../queue');
 
-load('../../quic_project_v2_data/celltype.mat')
+load('../../quic_project_v3_data/celltype.mat')
 obs = celltype{1}(:,:,z)==0;
 n = size(obs,1);
 
@@ -32,19 +32,19 @@ for i=1:n
 end
 obs = inf_obs;
 
-load('../../quic_project_v2_data/quic_project_v2_samp001/velocity.mat')
+load('../../quic_project_v3_data/quic_project_v3_samp005/velocity.mat')
 wind(:,:,1) = velocity.u{1}(:,:,z);
 wind(:,:,2) = velocity.v{1}(:,:,z);
 wind(:,:,3) = velocity.w{1}(:,:,z);
 
-speeds = [-2 -1 0 1 2];
+speeds = [-3 -2 -1 0 1 2 3];
 %start = [80 22 0 0]';
-start = [25 25 0 0]';
-goal = [48 25 0 0]';
+start = [13 19 0 0]';
+goal = [13 37 0 0]';
 % start = [15 75 0 0]';
 % goal = [25 10 0 0]';
 
-figure(75);
+figure(74);
 hold on;
 imagesc(obs);
 plot(start(1),start(2),'bo');
@@ -67,12 +67,16 @@ end
 figure(75);
 hold on;
 vtraj = sqrt(sum(xtraj(3:4,:).^2,1));
-%imagesc(obs*max(vtraj)*1.5);
 imagesc(velocity.mag{1}(:,:,z));
 scatter(xtraj(1,:),xtraj(2,:),25,vtraj,'filled');
+scatter(start(1),start(2),100,'g','filled');
+scatter(goal(1),goal(2),100,'r','filled');
+title('Minimum-Energy Trajectory in Sample Wind Field')
+xlabel('X [m]')
+ylabel('Y [m]')
 xlim([0 n]);
 ylim([0 n]);
-axis equal;
+hold off
 
 figure(76);
 hold on
@@ -85,5 +89,10 @@ legend('Vg','Va')
 hold off
 
 figure(77);
+hold on
 plot(1:numel(gtraj),gtraj,1:numel(htraj),htraj,'*');
-legend('G traj', 'H traj');
+title('Planner Cost Vs. Steps')
+ylabel('Cost')
+xlabel('Steps')
+legend('G', 'H');
+hold off
